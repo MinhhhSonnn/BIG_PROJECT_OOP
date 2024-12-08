b40bcac6f94a10739b81030c06a414a14fa4014b
luongvuthe <23020159@vnu.edu.vn>
2024-12-08 11:04:49 +0700

    the7

--- src/main/java/org/example/btl/Main.java
+++ src/main/java/org/example/btl/Main.java
@@ -38,6 +38,7 @@ public class Main extends Application {
     stage.setTitle("UET Library Management");
     stage.setScene(scene);
     stage.show();
+
   }
 
   public static void main(String[] args) {
