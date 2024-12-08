b9b8338e92945f49d8550f48c1b63642e87d5534
luongvuthe <23020159@vnu.edu.vn>
2024-12-08 16:31:07 +0700

    the7

--- src/main/java/org/example/btl/Controllers/myBookController.java
+++ src/main/java/org/example/btl/Controllers/myBookController.java
@@ -4,23 +4,32 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class dashboardUserController {
+public class myBookController {
+  @FXML
+  private HBox dashboardUserButton;
+
   @FXML
   private HBox listBookButton;
 
   @FXML
-  private Button logOutButton;
-
-  @FXML
   private HBox myBookButton;
 
-  @FXML
-  private Label userNameTextField;
+  public void dashboardUserView(){
+    try {
+      Stage stage = (Stage) listBookButton.getScene().getWindow();
+      Parent root = FXMLLoader.load(getClass().getResource("/org/example/btl/dashboardUser.fxml"));
+      Scene scene = new Scene(root);
+      stage.setResizable(false); // tat nut maximine
+      stage.setTitle("UET Library Management");
+      stage.setScene(scene);
+      stage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
   public void listBookView(){
     try {
@@ -40,20 +49,6 @@ public class dashboardUserController {
     try {
       Stage stage = (Stage) myBookButton.getScene().getWindow();
       Parent root = FXMLLoader.load(getClass().getResource("/org/example/btl/myBook.fxml"));
-      Scene scene = new Scene(root);
-      stage.setResizable(false); // tat nut maximine
-      stage.setTitle("UET Library Management");
-      stage.setScene(scene);
-      stage.show();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  public void logOut(){
-    try {
-      Stage stage = (Stage) listBookButton.getScene().getWindow();
-      Parent root = FXMLLoader.load(getClass().getResource("/org/example/btl/login.fxml"));
       Scene scene = new Scene(root);
       stage.setResizable(false); // tat nut maximine
       stage.setTitle("UET Library Management");
